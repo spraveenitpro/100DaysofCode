07af57d60c20d29228d2a52e66c8d9c8cf71ae8a
Praveen <spraveenitpro@gmail.com>
2020-08-06 22:18:22 -0700

    Setup event handling for front page

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+  gotoStore = (event) => {
+    // Stop the form from submitting
+    event.preventDefault();
+    // Get Text from input
+    const storeName = this.myInput.current.value;
+    //Direct the page to /store/what-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.gotoStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Enter Store" />
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Enter Store"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store </button>
       </form>
     );

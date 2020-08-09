23c2dc67e1756067587c83f7fefd4026c7480149
Praveen <spraveenitpro@gmail.com>
2020-08-09 00:21:56 -0700

    scafolded expressjs application

--- flashcards/app.js
+++ flashcards/app.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("<h1>It works!</h1>");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<h1>Hello World!!</h1>");
+});
+app.listen(3000, () => {
+  console.log("application is running on localhost");
+});

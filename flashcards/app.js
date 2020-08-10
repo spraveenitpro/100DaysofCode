6dfeaea9f0842fe8a5743acf096ff8626b4cb4ab
Praveen <spraveenitpro@gmail.com>
2020-08-10 00:28:07 -0700

    Daily update

--- flashcards/app.js
+++ flashcards/app.js
@@ -5,8 +5,12 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<h1>Hello World!!</h1>");
+app.get("/cards", (req, res) => {
+  //res.locals.prompt = "Who is burried in Grants tomb";
+  res.render("card", {
+    prompt: "Who is burried in Grants tomb",
+    hint: "Think about whose tomb it is",
+  });
 });
 app.listen(3000, () => {
   console.log("application is running on localhost");

82badd123910944c44287d073cdfd254d9b7a097
Praveen <spraveenitpro@gmail.com>
2020-08-10 00:55:29 -0700

    Daily update

--- flashcards/app.js
+++ flashcards/app.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const app = express();
+const colors = ["red", "orange", "yellow", "green", "green", "blue", "purple"];
 app.set("view engine", "pug");
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+// /Sandbox
+// FirstName | LastName
 
 app.get("/cards", (req, res) => {
   //res.locals.prompt = "Who is burried in Grants tomb";
   res.render("card", {
     prompt: "Who is burried in Grants tomb",
     hint: "Think about whose tomb it is",
+    colors,
   });
 });
 app.listen(3000, () => {

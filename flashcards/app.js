314864ec3aa07b6cf5bfb1b25a59f909dd3bf97d
Praveen <spraveenitpro@gmail.com>
2020-08-11 23:16:03 -0700

    Completed Error handling

--- flashcards/app.js
+++ flashcards/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 const colors = ["red", "orange", "yellow", "green", "green", "blue", "purple"];
 const names = [
   { FirstName: "Praveen", LastName: "selva" },
@@ -11,7 +14,22 @@ const names = [
   { FirstName: "Brandon", LastName: "dor" },
   { FirstName: "Liorti", LastName: "stick" },
 ];
+
 app.set("view engine", "pug");
+
+// app.use((req, res, next) => {
+//   //req.message = "This Message made it";
+//   console.log("hello");
+//   const err = new Error("Oh Noes!!");
+//   err.status = 500;
+//   next(err);
+// });
+
+app.use((req, res, next) => {
+  console.log("world!");
+  next();
+});
+
 app.get("/", (req, res) => {
   const name = req.cookies.username;
   if (name) {
@@ -20,9 +38,6 @@ app.get("/", (req, res) => {
     res.redirect("/hello");
   }
 });
-
-// /Sandbox
-// FirstName | LastName
 
 app.get("/cards", (req, res) => {
   //res.locals.prompt = "Who is burried in Grants tomb";
@@ -57,6 +72,19 @@ app.post("/hello", (req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("/");
 });
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render("error");
+});
+
 app.listen(3000, () => {
   console.log("application is running on localhost");
 });

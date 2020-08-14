0c31704e9ade8e110b41016c596ed9702f3f0b22
Praveen <spraveenitpro@gmail.com>
2020-08-13 23:51:01 -0700

    Added the GET routes to the express REST API app

--- express_api/app.js
+++ express_api/app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const app = express();
 
+const records = require("./records");
+
 // Send a GET request to  /quotes to READ a list of quotes
 
-app.get("/quotes", (req, res) => {
-  res.json(data);
+app.get("/quotes", async (req, res) => {
+  const quotes = await records.getQuotes();
+  res.json(quotes);
 });
 
 // Send a GET request to /quotes/:id [Read a quote]
 
-app.get("/quotes/:id", (req, res) => {
+app.get("/quotes/:id", async (req, res) => {
   //console.log(req.params.id);
 
-  const quote = data.quotes.find((quote) => quote.id == req.params.id);
+  const quote = await records.getQuote(req.params.id);
   res.json(quote);
 });
 
@@ -24,26 +27,3 @@ app.get("/quotes/:id", (req, res) => {
 
 // Send a GET request to READ random quote
 app.listen(3000, () => console.log("Quote API listening on port 3000!"));
-
-const data = {
-  quotes: [
-    {
-      id: 8721,
-      quote:
-        "We must accept finite disappointment, but we must never lose infinite hope.",
-      author: "Martin Luther King",
-    },
-    {
-      id: 5779,
-      quote:
-        "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
-      author: "Yvonne Pierre",
-    },
-    {
-      id: 3406,
-      quote:
-        "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
-      author: "Barbara DeAngelis",
-    },
-  ],
-};

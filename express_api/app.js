cb543c4c32e98cf1ec0786cb0482eda402c7246f
Praveen <spraveenitpro@gmail.com>
2020-08-15 00:15:36 -0700

    completed create/read/update/delete quote routes

--- express_api/app.js
+++ express_api/app.js
@@ -7,8 +7,12 @@ app.use(express.json());
 // Send a GET request to  /quotes to READ a list of quotes
 
 app.get("/quotes", async (req, res) => {
-  const quotes = await records.getQuotes();
-  res.json(quotes);
+  try {
+    const quotes = await records.getQuotes();
+    res.json(quotes);
+  } catch (err) {
+    res.json({ message: err.message });
+  }
 });
 
 // Send a GET request to /quotes/:id [Read a quote]
@@ -16,23 +20,72 @@ app.get("/quotes", async (req, res) => {
 app.get("/quotes/:id", async (req, res) => {
   //console.log(req.params.id);
 
-  const quote = await records.getQuote(req.params.id);
-  res.json(quote);
+  try {
+    const quote = await records.getQuote(req.params.id);
+    if (quote) {
+      res.json(quote);
+    } else {
+      res.status(404).json({ message: "Quote is not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // Send a POST request [Create]
 
-app.post("/quotes", (req, res) => {
-  const quote = records.createQuote({
-    quote: req.body.quote,
-    author: req.body.author,
-  });
-  res.json(quote);
+app.post("/quotes", async (req, res) => {
+  try {
+    //throw new Error("Oh no, another error");
+    if (req.body.author && req.body.quote) {
+      const quote = await records.createQuote({
+        quote: req.body.quote,
+        author: req.body.author,
+      });
+      res.status(201).json(quote);
+    } else {
+      res.status(400).json({ message: "Quote and Author required" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // Send a PUT request [Update]
 
+app.put("/quotes/:id", async (req, res) => {
+  try {
+    const quote = await records.getQuote(req.params.id);
+    if (quote) {
+      quote.quote = req.body.quote;
+      quote.author = req.body.author;
+
+      await records.updateQuote(quote);
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Quote not found!" });
+    }
+    records.updateQuote();
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 // Send a DELETE request [Delete]
+
+app.delete("/quotes/:id", async (req, res) => {
+  try {
+    const quote = await records.getQuote(req.params.id);
+    if (quote) {
+      await records.deleteQuote(quote);
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Quote not found!" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // Send a GET request to READ random quote
 app.listen(3000, () => console.log("Quote API listening on port 3000!"));

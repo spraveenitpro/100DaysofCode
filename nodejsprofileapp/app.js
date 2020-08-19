c6924a8eafed74dc8ae2f25c759046cf1badfd5a
Praveen <spraveenitpro@gmail.com>
2020-08-19 00:47:12 -0700

    Scaffolding NODEJS website for retrieving API data

---
+++ nodejsprofileapp/app.js
@@ -0,0 +1,9 @@
+// 1. Create a web server
+
+// 2. Handle HTTP route GET /
+
+// 3. Handle POST route for /
+
+// 4. Handle HTTP route for /:username
+
+// 5. Function that handles the reading of files and merge in value

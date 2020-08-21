f163b9dcdc4a9c4d06f56b2078ca99a5accb1b42
Praveen <spraveenitpro@gmail.com>
2020-08-21 00:14:53 -0700

    Getting started with Sequalize and models

--- sql-orms-with-node/app.js
+++ sql-orms-with-node/app.js
@@ -2,13 +2,42 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "movies.db",
+  logging: false,
 });
 
-(async () => {
+/* (async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection to the database is successful");
   } catch (error) {
     console.log("Connection to the database is not successful");
+  }
+})(); */
+
+// Movie model
+
+class Movie extends Sequelize.Model {}
+Movie.init(
+  {
+    title: Sequelize.STRING,
+  },
+  { sequelize }
+);
+
+(async () => {
+  await sequelize.sync({ force: true });
+  try {
+    const movieInstances = await Promise.all([
+      Movie.create({
+        title: "Toy Story",
+      }),
+      Movie.create({
+        title: "The Incredibles",
+      }),
+    ]);
+    const moviesJSON = movieInstances.map((movie) => movie.toJSON());
+    console.log(moviesJSON);
+  } catch (error) {
+    console.log("Error connecting to DB!");
   }
 })();

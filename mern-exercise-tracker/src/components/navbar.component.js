6ef81d175efbad8cd6d1807572273d16f9505df7
Praveen <spraveenitpro@gmail.com>
2020-08-31 23:06:05 -0700

    migrating from class to functions

--- mern-exercise-tracker/src/components/navbar.component.js
+++ mern-exercise-tracker/src/components/navbar.component.js
@@ -1,36 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link to="/" className="navbar-brand">
-          Exercise Tracker
-        </Link>
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link to="/" className="navbar-brand">
+        Exercise Tracker
+      </Link>
 
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Exercises
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/create" className="nav-link">
-                Create Exercises Log
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/user" className="nav-link">
-                Create User
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+      <div className="collapse navbar-collapse">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Exercises
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/create" className="nav-link">
+              Create Exercises Log
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/user" className="nav-link">
+              Create User
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;

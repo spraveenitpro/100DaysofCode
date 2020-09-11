7883684ce10398195cd062c5f759488e86227ff1
Praveen <spraveenitpro@gmail.com>
2020-09-10 22:54:35 -0700

    Completed Displaying list of Daily notes

--- dailyjournal/backend/app.js
+++ dailyjournal/backend/app.js
@@ -4,10 +4,11 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+
 const uri = process.env.ATLAS_URI;
 
 mongoose.connect(uri, {
--- dailyjournal/src/components/daily-list.component.js
+++ dailyjournal/src/components/daily-list.component.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
+/* import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function DailyList() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
   useEffect(() => {
     axios
       .get("http://localhost:6000/daily/")
       .then((res) => {
-        setData(res.data);
+        console.log(res);
       })
       .catch((error) => {
         console.log(error);
@@ -17,4 +17,64 @@ function DailyList() {
   return <p>This is the list of Dailies</p>;
 }
 
-export default DailyList;
+export default DailyList; */
+
+import React, { Component } from "react";
+import axios from "axios";
+
+const Daily = (props) => (
+  <tr>
+    <td>{props.daily.date}</td>
+    <td>{props.daily.mood}</td>
+    <td>{props.daily.note}</td>
+  </tr>
+);
+
+class Dailies extends Component {
+  state = {
+    dailies: [],
+  };
+
+  componentDidMount() {
+    this.getDaily();
+  }
+
+  getDaily = () => {
+    axios
+      .get("http://localhost:3001/daily")
+      .then((res) => {
+        if (res.data) {
+          this.setState({ dailies: res.data });
+          console.log(res.data);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  dailyList() {
+    return this.state.dailies.map((currentdaily) => {
+      return <Daily daily={currentdaily} key={currentdaily._id} />;
+    });
+  }
+
+  render() {
+    let { dailies } = this.state;
+    return (
+      <div>
+        <h1> Daily Journal</h1>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Date</th>
+              <th>Mood</th>
+              <th>Note</th>
+            </tr>
+          </thead>
+          <tbody>{this.dailyList()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Dailies;

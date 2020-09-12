f6d993cfdd2ade2dc88ce72bca4c52e71a970719
Praveen <spraveenitpro@gmail.com>
2020-09-11 23:44:16 -0700

    Rewriting dailyJournal to use hooks

--- dailyjournal/src/components/create-daily.component.js
+++ dailyjournal/src/components/create-daily.component.js
@@ -1,48 +1,72 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-class CreateDaily extends Component {
-  state = {
+function CreateDaily() {
+  /*   state = {
     date: new Date(),
     mood: "",
     notes: "",
+  }; */
+
+  const [values, setValues] = useState({
+    date: new Date(),
+    mood: "",
+    notes: "",
+  });
+
+  const handleMoodChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      mood: event.target.value,
+    }));
   };
 
-  render() {
-    return (
-      <div className="container">
-        <form>
-          <div className="form-group">
-            <label for="exampleFormControlInput1">Mood</label>
-            <input
-              type="text"
-              onchange={this.handleChange}
-              className="form-control"
-              id="exampleFormControlInput1"
-              placeholder="Enter how you feel"
-              value={this.state.mood}
-            />
-          </div>
+  const handleNoteChange = (event) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      notes: event.target.value,
+    }));
+  };
 
-          <div className="form-group">
-            <label for="exampleFormControlTextarea1">
-              Enter the highlights for the day
-            </label>
-            <textarea
-              className="form-control"
-              id="exampleFormControlTextarea1"
-              rows="3"
-              onchange={this.handleChange}
-              value={this.state.notes}
-            ></textarea>
-          </div>
-          <button type="submit" class="btn btn-primary">
-            Submit
-          </button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="container">
+      <form>
+        <div className="form-group">
+          <label for="exampleFormControlInput1">Mood</label>
+          <input
+            type="text"
+            onChange={handleMoodChange}
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="Enter how you feel"
+            value={values.mood}
+          />
+        </div>
+
+        <div className="form-group">
+          <label for="exampleFormControlTextarea1">
+            Enter the highlights for the day
+          </label>
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            onChange={handleNoteChange}
+            value={values.notes}
+          ></textarea>
+        </div>
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Enter Daily Log"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CreateDaily;

30cebc47756cc0a1cc4d1b99e36a54475b90b048
Praveen <spraveenitpro@gmail.com>
2020-09-14 23:33:45 -0700

    Applied ReactStrap

--- contact-cards/src/App.js
+++ contact-cards/src/App.js
@@ -1,4 +1,17 @@
 import React, { useEffect, useState } from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  CardHeader,
+  Cardfooter,
+  CardFooter,
+  CardGroup,
+} from "reactstrap";
 import "./index.css";
 
 const App = () => {
@@ -21,14 +34,36 @@ const App = () => {
 
   return (
     <>
-      {contacts.map((contact) => (
-        <ContactCard
-          avatar={contact.picture.large}
-          name={contact.name.first + " " + contact.name.last}
-          email={contact.email}
-          age={contact.dob.age}
-        />
-      ))}
+      <CardGroup>
+        {contacts.map((contact) => (
+          <ContactCard
+            avatar={contact.picture.large}
+            name={contact.name.first + " " + contact.name.last}
+            email={contact.email}
+            age={contact.dob.age}
+          />
+        ))}
+      </CardGroup>
+      <CardGroup>
+        {contacts.map((contact) => (
+          <ContactCard
+            avatar={contact.picture.large}
+            name={contact.name.first + " " + contact.name.last}
+            email={contact.email}
+            age={contact.dob.age}
+          />
+        ))}
+      </CardGroup>
+      <CardGroup>
+        {contacts.map((contact) => (
+          <ContactCard
+            avatar={contact.picture.large}
+            name={contact.name.first + " " + contact.name.last}
+            email={contact.email}
+            age={contact.dob.age}
+          />
+        ))}
+      </CardGroup>
     </>
   );
 };
@@ -37,21 +72,39 @@ const ContactCard = (props) => {
   const [showAge, setShowAge] = useState(false);
   console.log(props.avatar);
   return (
-    <div className="contact-card">
-      <img src={props.avatar} alt="" />
-      <div className="user-details">
-        <p>Name: {props.name}</p>
-        <p>Email:{props.email}</p>
+    // <div className="contact-card">
+    //   <img src={props.avatar} alt="" />
+    //   <div className="user-details">
+    //     <p>Name: {props.name}</p>
+    //     <p>Email:{props.email}</p>
+    //     <button
+    //       onClick={() => {
+    //         setShowAge(!showAge);
+    //       }}
+    //     >
+    //       Toggle Age
+    //     </button>
+    //     {showAge && <p> Age: {props.age}</p>}
+    //   </div>
+    // </div>
+
+    <Card body outline color="primary" className="text-center">
+      <CardBody>
+        <CardHeader>{props.name}</CardHeader>
+        <img top width="128px" src={props.avatar} alt="profile pic" />
+
+        <CardText> {showAge && <p> Age: {props.age}</p>}</CardText>
         <button
+          className="btn btn-primary"
           onClick={() => {
             setShowAge(!showAge);
           }}
         >
           Toggle Age
         </button>
-        {showAge && <p> Age: {props.age}</p>}
-      </div>
-    </div>
+      </CardBody>
+      <CardFooter> {props.email} </CardFooter>
+    </Card>
   );
 };
 

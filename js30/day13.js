68020d223768ef4f5c89ef1f7ead55b0e9c9e1cb
spraveenitpro <spraveenitpro@gmail.com>
2020-12-17 13:00:20 -0800

    daily update

---
+++ js30/day13.js
@@ -0,0 +1,12 @@
+class MyBook extends Book {
+  constructor(title, author, price) {
+    super(title, author);
+    this.price = price;
+  }
+
+  display() {
+    console.log(
+      `Title: ${this.title}\nAuthor: ${this.author}\nPrice: ${this.price}`
+    );
+  }
+}
